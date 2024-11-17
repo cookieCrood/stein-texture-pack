a96c2c9f23d75dae96cceecd385f96bf81da8bef
cookieCrood <139991505+cookieCrood@users.noreply.github.com>
2024-11-17 18:16:21 +0100

    Create test-pack.js

---
+++ test-pack.js
@@ -0,0 +1,9 @@
+function addStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
